aec4124b46abc49c268fcc7088bdd2ff7364a9a4
Dedrick Koh <dedrickkoh1@gmail.com>
2026-05-22 20:45:09 +0800

    fix unused window variable error when building in release

--- imgui_impl_rgfw.h
+++ imgui_impl_rgfw.h
@@ -399,6 +399,7 @@ void ImGui_ImplRgfw_InstallCallbacks(RGFW_window* window)
     ImGui_ImplRgfw_Data* bd = ImGui_ImplRgfw_GetBackendData();
     IM_ASSERT(bd->InstalledCallbacks == false && "Callbacks already installed!");
     IM_ASSERT(bd->Window == window);
+    IM_UNUSED(window);
 
     /*
         TODO: RGFW doesn't have anyway to do this yet
@@ -414,6 +415,7 @@ void ImGui_ImplRgfw_RestoreCallbacks(RGFW_window* window)
     ImGui_ImplRgfw_Data* bd = ImGui_ImplRgfw_GetBackendData();
     IM_ASSERT(bd->InstalledCallbacks == true && "Callbacks not installed!");
     IM_ASSERT(bd->Window == window);
+    IM_UNUSED(window);
 
     for (u32 i = RGFW_eventNone + 1; i < RGFW_eventCount; i++)
         RGFW_setEventCallback((RGFW_eventType)i, bd->PrevCallbacks.arr[i]);
